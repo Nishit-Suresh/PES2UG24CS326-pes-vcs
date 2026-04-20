6253b0b7b9443cd465a6dc7f3f55e4e97c4db81f
Nishit Suresh <nishisureshis@gmail.com>
2026-04-20 16:02:04 +0530

    Phase 4: added tree creation and parent handling in commit_create

--- commit.c
+++ commit.c
@@ -195,5 +195,13 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
+    Commit c;
+    memset(&c, 0, sizeof(c));
+   if (tree_from_index(&c.tree) != 0) return -1;
+    if (head_read(&c.parent) == 0) {
+    c.has_parent = 1;
+    } else {
+        c.has_parent = 0;
+    }
     return -1;
 }
